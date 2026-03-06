93a7525566c0be8844d6634d3d1551561f69bbd8
pkth-18 <princesskathrinetiocao@gmail.com>
2026-03-06 22:51:42 +0800

    update file

--- api/chatbot copy.js
+++ api/chatbot copy.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
     );
 
     const data = await response.json();
-    res.status(200).json({ reply: "Hello from backend!" });
+    res.status(200).json({ reply: data.output_text || "No response" });
   } catch (err) {
     console.error(err);
     res.status(500).json({ reply: "Error connecting to AI API" });
